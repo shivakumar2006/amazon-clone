cc9f7474a2726171c9049d9507b93eac3ea3b440
shivakumar2006 <official.shivakumar06@gmail.com>
2025-02-21 15:53:50 +0530

    add react-lazy-image

--- src/components/ShopSection.jsx
+++ src/components/ShopSection.jsx
@@ -21,91 +21,69 @@ import clothes4 from '../assets/clothes4.jpg';
 import makeup2 from '../assets/makeup2.jpg';
 import makeup3 from '../assets/makeup3.jpg';
 import makeup1 from '../assets/makeup1.jpg';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css'; 
 
 const ShopSection = () => {
+
+    const images = [
+        { src: pencil, alt: "New Year, new supplies", linkText: "See more" },
+        { src: heart, alt: "Shop Valentine's Day gifts", linkText: "Shop Now" },
+        { src: console, alt: "Get your game on", linkText: "Shop gaming" },
+        { src: toys, alt: "Toys & more", linkText: "Shop Now" },
+        { src: furniture, alt: "Premium Furnitures", linkText: "See more"},
+        { src: lamps, alt: "Lamps & more", linkText: "Shop Lamps"},
+        { src: paintings, alt: "Get Beautiful Paintings", linkText: "Explore more"},
+        { src: skinCare, alt: "Buy your skin", linkText: "Explore your favourites"},
+    ]
+
+    const images2 = [
+        { src: phone, alt: "Phone"},
+        { src: keyboard, alt: "Keyboard"},
+        { src: laptop1, alt: "Laptop1"},
+        { src: laptop2, alt: "laptop2"},
+        { src: headphone, alt: "Headphone"},
+    ]
+
     return (
         <>
         <div className='shop-section'>
-            <div className='box1 box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px' }}>New year, new supplies</h2>
+            {images.map((image, index) => (
+            <div className='box' key={index}>
+                <h2 style={{ marginTop: '15px', marginLeft: '20px' }}>{image.alt}</h2>
                 <div className="box1_img">
-                    <img className="box1_img" src={pencil} alt="" />
+                   <LazyLoadImage 
+                        src={image.src}
+                        alt={image.alt}
+                        effect='blur'
+                        width="100%"
+                        height="310px"
+                        object-fit="cover"
+                   />
                 </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px' }}><Link>See more</Link></p>
+                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}>
+                    <Link>{image.linkText}</Link>
+                </p>
             </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Shop Valentine's Day gifts</h2>
-                <div className="box1_img">
-                    <img className='box1_img' src={heart} alt="product" />
-                </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}><Link>Shop Now</Link></p>
-            </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Get your game on</h2>
-                <div className="box1_img">
-                    <img className='box1_img' src={console} alt='product' />
-                </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}><Link>Shop gaming</Link></p>
-            </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Toys & more</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={toys} alt='product' />
-                </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}><Link>Shop Now</Link></p>
-            </div>
-        </div>
-
-        <div className='shop-section'>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Premium Furnitures</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={furniture} alt='product' />
-                    <p className='para' style={{ marginTop: '0px', marginLeft: '9px'}}><Link>See more</Link></p>
-                </div>
-            </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Lamps & more</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={lamps} alt="product" />
-                    <p className='para' style={{ marginTop: '0px', marginLeft: '9px'}}><Link>Shop lamps</Link></p>
-                </div>
-            </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Get Beautiful Paintings</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={paintings} alt='product' />
-                </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}><Link>Explore more</Link></p>
-            </div>
-            <div className='box'>
-                <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Buy your skin care</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={skinCare} alt='product' />
-                </div>
-                <p className='para' style={{ marginTop: '18px', marginLeft: '20px'}}><Link>Explore your favourites</Link></p>
-            </div>
+            ))}
         </div>
 
         <div className='banner-section'>
             <div className='banner-box'>
                 <h2 style={{marginTop: '20px', marginLeft: '10px'}}>Best Electronics items</h2>
                 <div className='banner-outer-box'>
-                    <div className='banner-inner-box'>
-                        <img className='banner-img' src={phone} alt='product' />
+                    {images2.map((image, index) => (
+                    <div className='banner-inner-box' key={index}>
+                        <LazyLoadImage 
+                            className="banner-img"
+                            src={image.src}
+                            alt={image.alt}
+                            effect="blur"
+                            height="100%"
+                            width="100%"
+                        />
                     </div>
-                    <div className='banner-inner-box'>
-                        <img className='banner-img' src={keyboard} alt='product' />
-                    </div>
-                    <div className='banner-inner-box'>
-                        <img className='banner-img' src={laptop1} alt='product' />
-                    </div>
-                    <div className='banner-inner-box'>
-                        <img className='banner-img' src={laptop2} alt='product' />
-                    </div>
-                    <div className='banner-inner-box'>
-                        <img className='banner-img' src={headphone} alt='product' />
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
@@ -113,8 +91,8 @@ const ShopSection = () => {
         <div className='shop-section'>
             <div className='box1 box'>
                 <h2 style={{ marginTop: '15px', marginLeft: '20px' }}>New year, new supplies</h2>
-                <div className="box1_img">
-                    <img className="box1_img" src={pencil} alt="product" />
+                <div className="box1_img" style={{width: "100%", marginLeft: "-1px"}}>
+                    <LazyLoadImage className="box1_img" src={pencil} alt="product" effect="blur"/>
                 </div>
                 <p className='para' style={{ marginTop: '18px', marginLeft: '20px' }}><Link>See more</Link></p>
             </div>
@@ -122,11 +100,11 @@ const ShopSection = () => {
                 <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Makeup kits</h2>
                 <div className='multiple-box'>
                     <div className="multiple-inner-box">
-                        <img className='img1' src={makeup3} alt="product" />
-                        <img className='img1' src={makeup2} alt='product' />
+                        <LazyLoadImage className='img1' src={makeup3} alt="product" effect="blur" />
+                        <LazyLoadImage className='img1' src={makeup2} alt='product' effect="blur" />
                     </div>
                     <div className='multiple-inner-second'>
-                        <img className='img2' src={makeup1} alt='product' />
+                        <LazyLoadImage className='img2' src={makeup1} alt='product' effect="blur" />
                     </div>
                 </div>
                 <p className='para' style={{ marginTop: '12px', marginLeft: '20px'}}><Link>Shop Now</Link></p>
@@ -135,20 +113,20 @@ const ShopSection = () => {
                 <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Go with trends</h2>
                 <div className="multiple-box">
                     <div className='multiple-inner-box'>
-                    <img className='img1' src={clothes1} alt='product' />
-                    <img className='img1' src={clothes2} alt='product' />
+                    <LazyLoadImage className='img1' src={clothes1} alt='product' effect="blur" />
+                    <LazyLoadImage className='img1' src={clothes2} alt='product' effect="blur" />
                     </div>
                     <div className='multiple-inner-box'>
-                    <img className='img1' src={clothes3} alt='product' />
-                    <img className='img1' src={clothes4} alt='product' />
+                    <LazyLoadImage className='img1' src={clothes3} alt='product' effect="blur" />
+                    <LazyLoadImage className='img1' src={clothes4} alt='product' effect="blur" />
                     </div>
                 </div>
                 <p className='para' style={{ marginTop: '10px', marginLeft: '16px'}}><Link>Shop clothes</Link></p>
             </div>
             <div className='box'>
                 <h2 style={{ marginTop: '15px', marginLeft: '20px'}}>Buy Mirrors</h2>
-                <div className='box1_img'>
-                    <img className='box1_img' src={mirror} alt='product' />
+                <div className='box1_img' style={{width: "100%", marginLeft: "-1px"}}>
+                    <LazyLoadImage className='box1_img' src={mirror} alt='product' effect="blur" />
                 </div>
                 <p className='para' style={{ margin: '20px'}}><Link>Shop Now</Link></p>
             </div>
