d795c29e4d26f88bb9e24e3b9c7214a69d21408d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-02-22 15:26:15 +0530

    saved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,16 @@ import books from '../assets/books.jpg';
 import beauty from '../assets/beauty.jpg';
 import kitchen from '../assets/kitchen.jpg';
 import toys from '../assets/toys.jpg';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css'; 
 
-const images = [valentine, books, beauty, kitchen, toys]
+const images = [
+    {src: valentine}, 
+    {src: books}, 
+    {src: beauty}, 
+    {src: kitchen}, 
+    {src: toys}
+]
 
 const Hero = () => {
  
@@ -26,12 +34,17 @@ const Hero = () => {
         <>
         <div className='hero-container'>
             {images.map((img, index) => (
-                <img
+                <div className={`hero-image ${index === currentIndex ? 'active' : ''}`}>
+                <LazyLoadImage
                     key={index}
-                    src={img}
+                    src={img.src}
                     alt="Amazon Hero"
-                    className={`hero-image ${index === currentIndex ? 'active' : ''}`}
+                    effect="blur"
+                    width="100%"
+                    height="auto"
+                    objectFit="cover"
                 />
+                </div>
             ))}
             <div className='hero-message'>
                 <p>You are on Amazon.com. You can also shop on Amazon India for Millions of products with fast local delivery.<Link to="https://www.amazon.in/">Click here to go to amazon.in</Link></p>
